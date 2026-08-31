66199428ced7fbb090d069d019cefd5a5b4435f8
sam <samkshawani1@gmail.com>
2026-08-31 16:11:28 +0530

    Done

--- foodkart.py
+++ foodkart.py
@@ -2,6 +2,8 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.breakpoint = None
+        self.newLink = None
 
 
 class LinkedList:
@@ -28,6 +30,46 @@ class LinkedList:
             temp = temp.next
         print(None)
 
+    def solution(self):
+        kart2 = self.head
+        #counting elemets
+        count = 0
+        while kart2 is not None:
+            # print(temp.data, end=" - > ")
+            count += 1
+            kart2 = kart2.next
+        print(count)
+
+
+        #Dividing
+        stop = count / 2
+        count = 0
+        kart2 = self.head
+        print(count, stop)
+        while kart2 is not None:
+            if count == stop:
+                self.breakpoint = kart2.data
+                print("Break point: ",kart2.data)
+                return
+            count += 1
+            kart2 = kart2.next
+
+    def making(self):
+        temp = self.head
+
+        while temp is not None:
+            if(temp.next.data == self.breakpoint):
+                self.newLink = temp.next.next
+                temp.next = None
+            temp = temp.next
+            
+        temp = self.newLink
+        while temp is not None:
+            print(temp.data, end=" - > ")
+            temp = temp.next
+        print(None)
+                
+
 
 
 l = LinkedList()
@@ -37,9 +79,16 @@ l.insBeg(30)
 l.insBeg(40)
 l.insBeg(50)
 l.insBeg(60)
-l.insBeg(10)
+
+l.insBeg(210)
 l.insBeg(20)
 l.insBeg(30)
 l.insBeg(40)
 l.insBeg(50)
 l.insBeg(60)
+
+l.display()
+
+l.solution()
+l.making()
+l.display()
